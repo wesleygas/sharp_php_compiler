9181695eb41f88bf07a844c0a43e32eae68c58f4
wesleygas <wesleygl16@gmail.com>
2020-02-19 21:58:27 -0300

    class scaffolding

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace compylador
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            String code = args[0];
+            Console.WriteLine($"Codigo {code}");
+            return 0;
+        }
+    }
+}
---
+++ RaulException.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+[Serializable()]
+public class RaulException : System.Exception{
+    public RaulException() : base(){ }
+    public RaulException(string message) : base(message) {}
+
+
+    // A constructor is needed for serialization when an
+    // exception propagates from a remoting server to the client. 
+    protected RaulException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
